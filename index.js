5d8b6f203a49c37b11a98ca620a969284869b7f6
BohdanIvanyna <ivanyna.bohdan@student.uzhnu.edu.ua>
2026-05-20 23:42:24 +0300

    fix cors and host

--- index.js
+++ index.js
@@ -4,19 +4,13 @@ import express from "express";
 import tripRoutes from "./routes/tripRoutes.js";
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Твоя адреса фронтенду
-  }),
-);
+app.use(cors());
 
 app.use(express.json());
-
 app.use("/api", tripRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server starting on port ${PORT}`);
 });
